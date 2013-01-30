d9d0b8d944c0fda306c02c00c7e98ca611493c5f
Francisco Fernández Nogueira <francisco.fernandez.nogueira@gmail.com>
2013-01-30 20:12:10 +0100

    file schema, model & crud

---
+++ data/schema.mysql.sql
@@ -0,0 +1,14 @@
+CREATE TABLE file
+(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	name VARCHAR(128) NOT NULL,
+	extension VARCHAR(128) NOT NULL,
+	type VARCHAR(128) NOT NULL,
+	size INTEGER NOT NULL,
+	account_id INTEGER NOT NULL,
+	PRIMARY KEY (id),
+	INDEX FK_file_account (account_id ASC),
+	CONSTRAINT FK_file_account FOREIGN KEY (account_id)
+		REFERENCES account (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
---
+++ views/file/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this FileController */
+/* @var $data File */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('extension')); ?>:</b>
+	<?php echo CHtml::encode($data->extension); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('type')); ?>:</b>
+	<?php echo CHtml::encode($data->type); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('size')); ?>:</b>
+	<?php echo CHtml::encode($data->size); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('account_id')); ?>:</b>
+	<?php echo CHtml::encode($data->account_id); ?>
+	<br />
+
+
+</div>
